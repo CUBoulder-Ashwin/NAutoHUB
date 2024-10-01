4c4b819897516580febf6aa6a54af4907ae0c61f
Ashwin <cuboulder.ashwin@gmail.com>
2024-09-30 23:40:07 -0600

    Added Template functionality

--- NSOT/GUI/flask_app/rcn.py
+++ NSOT/GUI/flask_app/rcn.py
@@ -1,9 +1,13 @@
 import os
 import sys
 from flask import Flask, render_template, request, redirect, url_for
+from jinja2 import Environment, FileSystemLoader
 
 # Get the current directory of this script
 current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Path to 'templates' folder at NSOT level
+templates_dir = os.path.join(current_dir, '..', '..', 'templates')
 
 # Go up two levels and into the 'python-files' directory
 python_files_dir = os.path.join(current_dir, '..', '..', 'python-files')
@@ -16,6 +20,9 @@ from ping import ping_local, ping_remote
 from goldenConfig import generate_configs
 
 app = Flask(__name__)
+
+# Set up Jinja2 environment to load templates from 'NSOT/templates' folder
+env = Environment(loader=FileSystemLoader(templates_dir))
 
 @app.route("/")
 def homepage():
@@ -39,10 +46,99 @@ def add_device():
 def configure_device():
     if request.method == 'POST':
         device_id = request.form['device_id']
-        configuration = request.form['configuration']
-        # Add logic to configure the device
+        router_type = request.form['router_type']
+
+        # Fetching interface configurations
+        interfaces = []
+        interface_types = request.form.getlist('interface_type[]')
+        interface_numbers = request.form.getlist('interface_number[]')
+        interface_ips = request.form.getlist('interface_ip[]')
+        interface_masks = request.form.getlist('interface_mask[]')
+        switchports = request.form.getlist('switchport[]')
+
+        # Safeguard: Ensure all lists have the same length
+        max_len = max(len(interface_types), len(interface_numbers), len(interface_ips), len(interface_masks), len(switchports))
+
+        # Fill missing fields with default values
+        interface_ips += [None] * (max_len - len(interface_ips))
+        interface_masks += [None] * (max_len - len(interface_masks))
+        switchports += [None] * (max_len - len(switchports))
+
+        # Build the interface list
+        for i in range(max_len):
+            interfaces.append({
+                'type': interface_types[i],
+                'number': interface_numbers[i],
+                'ip': interface_ips[i] if switchports[i] != 'yes' else None,
+                'mask': interface_masks[i] if switchports[i] != 'yes' else None,
+                'switchport': switchports[i] == 'yes'
+            })
+
+        # Fetching OSPF configurations
+        ospf_process_ids = request.form.getlist('ospf_process_id[]')
+        ospf_networks = request.form.getlist('ospf_network[]')
+        ospf_areas = request.form.getlist('ospf_area[]')
+        ospf_redistribute_connected = request.form.getlist('ospf_redistribute_connected[]')
+        ospf_redistribute_bgp = request.form.getlist('ospf_redistribute_bgp[]')
+
+        ospf = {
+            'process_ids': ospf_process_ids,
+            'networks': [{'ip': ospf_networks[i], 'area': ospf_areas[i]} for i in range(len(ospf_networks))],
+            'redistribute_connected': len(ospf_redistribute_connected) > 0,
+            'redistribute_bgp': len(ospf_redistribute_bgp) > 0
+        }
+
+        # Fetching BGP configurations
+        bgp_asns = request.form.getlist('bgp_asn[]')
+        bgp_networks = request.form.getlist('bgp_network[]')
+        bgp_neighbors = request.form.getlist('bgp_neighbor[]')
+        bgp_remote_as = request.form.getlist('bgp_remote_as[]')
+        bgp_address_families = request.form.getlist('bgp_address_family[]')
+
+        bgp = {
+            'asn': bgp_asns[0],
+            'neighbors': [{'ip': bgp_neighbors[i], 'remote_as': bgp_remote_as[i]} for i in range(len(bgp_neighbors))],
+            'address_families': [{'type': bgp_address_families[i], 'networks': [bgp_networks[i]]} for i in range(len(bgp_address_families))]
+        }
+
+        # Fetching VLAN configurations
+        vlans = []
+        vlan_ids = request.form.getlist('vlan_id[]')
+        vlan_names = request.form.getlist('vlan_name[]')
+
+        for i in range(len(vlan_ids)):
+            vlans.append({
+                'id': vlan_ids[i],
+                'name': vlan_names[i]
+            })
+
+        # Depending on the router type, choose the correct Jinja template
+        template_name = f"{router_type}_router.j2"
+        template = env.get_template(template_name)
+
+        # Render the template with the user-provided data
+        configuration = template.render(
+            device_id=device_id,
+            interfaces=interfaces,
+            ospf=ospf,
+            bgp=bgp,
+            vlans=vlans
+        )
+
+        # Logic to save the rendered configuration to a file
+        output_dir = os.path.join(current_dir, 'configforTest')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        filename = f"{device_id}.cfg"
+        with open(os.path.join(output_dir, filename), 'w') as f:
+            f.write(configuration)
+
         return redirect(url_for('homepage'))
+
     return render_template("configure_device.html")
+
+
 
 @app.route("/tools", methods=["GET", "POST"])
 def tools():
@@ -84,7 +180,6 @@ def tools():
 
     return render_template("tools.html", ping_result=ping_result, config_result=config_result)
 
-# New routes for About and Contact pages
 @app.route("/about")
 def about():
     return render_template("about.html")
