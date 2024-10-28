8239b48b125f6e5ebe859cee78c0d2f37fd966e1
Ashwin <cuboulder.ashwin@gmail.com>
2024-10-28 11:23:33 -0600

    Added templates for individual protocols

--- NSOT/GUI/flask_app/rcn.py
+++ NSOT/GUI/flask_app/rcn.py
@@ -18,6 +18,8 @@ sys.path.append(os.path.abspath(python_files_dir))
 # Import your custom modules from 'python-files'
 from ping import ping_local, ping_remote
 from goldenConfig import generate_configs
+from generate_yaml import create_yaml_from_form_data
+from config_Gen import conf_gen  # Updated import for config generation
 
 app = Flask(__name__)
 
@@ -56,15 +58,12 @@ def configure_device():
         interface_masks = request.form.getlist('interface_mask[]')
         switchports = request.form.getlist('switchport[]')
 
-        # Safeguard: Ensure all lists have the same length
         max_len = max(len(interface_types), len(interface_numbers), len(interface_ips), len(interface_masks), len(switchports))
 
-        # Fill missing fields with default values
         interface_ips += [None] * (max_len - len(interface_ips))
         interface_masks += [None] * (max_len - len(interface_masks))
         switchports += [None] * (max_len - len(switchports))
 
-        # Build the interface list
         for i in range(max_len):
             interfaces.append({
                 'type': interface_types[i],
@@ -74,34 +73,83 @@ def configure_device():
                 'switchport': switchports[i] == 'yes'
             })
 
-        # Fetching OSPF configurations
+        # Fetching OSPF configurations with wildcard masks
+        ospf = None
         ospf_process_ids = request.form.getlist('ospf_process_id[]')
         ospf_networks = request.form.getlist('ospf_network[]')
+        ospf_wildcards = request.form.getlist('ospf_wildcard[]')  # Capture wildcard mask for OSPF
         ospf_areas = request.form.getlist('ospf_area[]')
         ospf_redistribute_connected = request.form.getlist('ospf_redistribute_connected[]')
         ospf_redistribute_bgp = request.form.getlist('ospf_redistribute_bgp[]')
 
         ospf = {
             'process_ids': ospf_process_ids,
-            'networks': [{'ip': ospf_networks[i], 'area': ospf_areas[i]} for i in range(len(ospf_networks))],
+            'networks': [
+                {'ip': ospf_networks[i], 'wildcard': ospf_wildcards[i], 'area': ospf_areas[i]}
+                for i in range(len(ospf_networks))
+            ],
             'redistribute_connected': len(ospf_redistribute_connected) > 0,
             'redistribute_bgp': len(ospf_redistribute_bgp) > 0
         }
 
-        # Fetching BGP configurations
+        # Fetching RIP configurations
+        rip = None
+        rip_versions = request.form.getlist('rip_version[]')
+        rip_networks = request.form.getlist('rip_network[]')
+        rip_redistribute_selected = request.form.get('rip_redistribute')  # Check if the redistribute checkbox is selected
+        rip_bgp_as = request.form.getlist('rip_bgp_as[]')
+        rip_bgp_metric = request.form.getlist('rip_bgp_metric[]')
+
+        if rip_versions:
+            rip = {
+                'version': rip_versions[0],
+                'networks': [{'ip': net} for net in rip_networks if net]  # Add networks only if non-empty
+            }
+
+            # Only include redistribution settings if the checkbox is selected and values are provided
+            if rip_redistribute_selected:
+                redistribute = {}
+                if rip_bgp_as and rip_bgp_as[0]:
+                    redistribute['as_number'] = rip_bgp_as[0]
+                if rip_bgp_metric and rip_bgp_metric[0]:
+                    redistribute['metric'] = int(rip_bgp_metric[0])
+
+                # Only add redistribute to RIP if we have at least one value
+                if redistribute:
+                    rip['redistribute'] = redistribute
+
+
+        # Fetching BGP configurations with network subnet
+        bgp = None
         bgp_asns = request.form.getlist('bgp_asn[]')
         bgp_networks = request.form.getlist('bgp_network[]')
         bgp_neighbors = request.form.getlist('bgp_neighbor[]')
         bgp_remote_as = request.form.getlist('bgp_remote_as[]')
         bgp_address_families = request.form.getlist('bgp_address_family[]')
 
-        bgp = {
-            'asn': bgp_asns[0],
-            'neighbors': [{'ip': bgp_neighbors[i], 'remote_as': bgp_remote_as[i]} for i in range(len(bgp_neighbors))],
-            'address_families': [{'type': bgp_address_families[i], 'networks': [bgp_networks[i]]} for i in range(len(bgp_address_families))]
-        }
+        if bgp_asns:
+            bgp = {
+                'asn': bgp_asns[0],
+                'neighbors': [
+                    {'ip': ip, 'remote_as': remote_as}
+                    for ip, remote_as in zip(bgp_neighbors, bgp_remote_as) if ip and remote_as
+                ],
+                'address_families': [
+                    {
+                        'type': af_type,
+                        'networks': [
+                            {'ip': net}
+                            for net in bgp_networks if net
+                        ]
+                    }
+                    for af_type in bgp_address_families if af_type  # Only add if address family type is non-empty
+                ]
+            }
+
+            print("Collected BGP Data:", bgp)
 
         # Fetching VLAN configurations
+        vlans = None
         vlans = []
         vlan_ids = request.form.getlist('vlan_id[]')
         vlan_names = request.form.getlist('vlan_name[]')
@@ -112,33 +160,18 @@ def configure_device():
                 'name': vlan_names[i]
             })
 
-        # Depending on the router type, choose the correct Jinja template
-        template_name = f"{router_type}_router.j2"
-        template = env.get_template(template_name)
+        # Create the YAML file with collected data
+        create_yaml_from_form_data(device_id=device_id, router_type=router_type, interfaces=interfaces, ospf=ospf, bgp=bgp, vlans=vlans, rip=rip)
 
-        # Render the template with the user-provided data
-        configuration = template.render(
-            device_id=device_id,
-            interfaces=interfaces,
-            ospf=ospf,
-            bgp=bgp,
-            vlans=vlans
-        )
-
-        # Logic to save the rendered configuration to a file
-        output_dir = os.path.join(current_dir, 'configforTest')
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-        filename = f"{device_id}.cfg"
-        with open(os.path.join(output_dir, filename), 'w') as f:
-            f.write(configuration)
+        # Call the function to generate the device configurations
+        try:
+            conf_gen()
+        except Exception as e:
+            print(f"Error generating configurations: {e}")
 
         return redirect(url_for('homepage'))
 
     return render_template("configure_device.html")
-
-
 
 @app.route("/tools", methods=["GET", "POST"])
 def tools():
