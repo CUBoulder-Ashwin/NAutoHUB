0652edb74ffc8db92336eaa8654a009bfae63d47
CUBoulder-Ashwin <cuboulder.ashwin@gmail.com>
2025-01-27 04:16:47 -0700

    Fixing jenkinsfile

--- pilot-config/pilot.sh
+++ pilot-config/pilot.sh
@@ -7,12 +7,22 @@ echo "Building Docker images for hosts and webserver..."
 sudo docker build -f pilot-config/Dockerfile_Hosts -t hosts:latest .
 sudo docker build -f pilot-config/Dockerfile_WebServer -t server:latest .
 
-# Add the netcfg file to /etc/netplan and apply
-echo "Creating netplan configs..."
-sudo cp pilot-config/netcfg.yaml /etc/netplan/02-netcfg.yaml
-sudo chmod 600 /etc/netplan/02-netcfg.yaml
-echo "Applying netplan..."
-sudo netplan apply
+# # Add the netcfg file to /etc/netplan and apply
+# echo "Creating netplan configs..."
+# sudo cp pilot-config/netcfg.yaml /etc/netplan/02-netcfg.yaml
+# sudo chmod 600 /etc/netplan/02-netcfg.yaml
+# echo "Applying netplan..."
+# sudo netplan apply
+
+#Create management network
+echo "Creating management network..."
+sudo ip link add eth0 type dummy
+sudo ip link add link eth0 name eth0.100 type vlan id 100
+sudo ip addr add 10.0.101.200/24 dev eth0.100
+sudo ip link set eth0 up
+sudo ip link set eth0.100 up
+sudo ip route add 10.0.100.0/24 via 10.0.101.100 dev eth0.100
+
 
 # Create a virtual environment and activate it
 echo "Setting up virtual environment..."
@@ -37,5 +47,7 @@ python3 pilot-config/pilot.py
 #Running NAutoHUB
 echo "Running NAutoHUB"
 python3 NSOT/GUI/flask_app/rcn.py
+
+
 
 echo "Setup complete!"
